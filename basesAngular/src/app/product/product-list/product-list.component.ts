5891518fbbb9f8183ce548b0c92cfe19909966db
Juan Andres Torres Gonzalez <jatg0101@gmail.com>
2024-09-20 20:54:07 -0600

    viernes 20

--- basesAngular/src/app/product/product-list/product-list.component.ts
+++ basesAngular/src/app/product/product-list/product-list.component.ts
@@ -7,7 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+//*ngIf=true o false
+
   productos:any[]=[
+   
     {
       "productoId":1,
       "Modelo": "Sentra",
@@ -16,7 +19,7 @@ export class ProductListComponent {
       "Year": 2023,
       "Marca": "Nissan",
       "Color": "Azul",
-      "ImagenUrl": ""
+      "ImagenUrl": "https://acroadtrip.blob.core.windows.net/catalogo-imagenes/m/RT_V_0c7166c16ff74073b65851f7f0b360ed.jpg"
     },
     {
       "productoId":2,
@@ -26,7 +29,7 @@ export class ProductListComponent {
       "Year": 2022,
       "Marca": "Audi",
       "Color": "Blanco",
-      "ImagenUrl": ""
+      "ImagenUrl": "https://mediaservice.audi.com/media/cdb/data/053e359d-d223-49ed-9b41-2346d1525ef1.jpg"
     },
     {
       "productoId":3,
@@ -36,7 +39,7 @@ export class ProductListComponent {
       "Year": 2023,
       "Marca": "Kia",
       "Color": "Negro",
-      "ImagenUrl": ""
+      "ImagenUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnEFAJK3u6QRkfaFyB9zkqp090pgsaCk8r3w&s"
     },
   ]
 
